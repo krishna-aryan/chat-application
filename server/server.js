bf35bed73a911cd8b4ede371c3cb740c2e740ffe
Krishna <bhagabatsahoo73@gmail.com>
2026-05-05 00:54:52 +0530

    Update app and server config, move background asset

--- server/server.js
+++ server/server.js
@@ -52,7 +52,12 @@ app.use('/api/status', (req, res) => res.send('Server is running'));
 app.use('/api/auth',userRouter);
 app.use('/api/messages',messageRouter);
 
-const PORT = process.env.PORT || 5000;
+
+if(process.env.NODE_ENV === 'production') {
+    
+    const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+}
+export default server;
